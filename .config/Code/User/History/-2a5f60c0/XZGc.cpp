59f0d76b1cfd4d406b86a4bcfae70702df717ec0
GratG <grantgroenewaldza@gmail.com>
2023-11-13 00:13:32 +0200

    first .config iteration

---
+++ .config/Code/User/History/-2a5f60c0/XZGc.cpp
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define SIZE 5
+
+int nums[SIZE] = {0, 1, 2, 3, 4};
---
+++ .config/Code/User/History/197a74e7/rJyO.cpp
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#define SIZE 5
+
+int nums[SIZE] = {0, 1, 2, 3, 4};
+
+int main(){
+
+
+    std::cout << "hi";
+
+    return 0;
+
+}
